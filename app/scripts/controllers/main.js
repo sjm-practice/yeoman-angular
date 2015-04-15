cd4be214b6aaa4a54298e617e469e71f450d9a3e
stevenjmarsh <sjm.atl@gmail.com>
2015-04-15 15:35:34 -0700

    todo list, add item

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,13 @@
  */
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.todos = [
+      'Item 1',
+      'Item 2',
+      'Item 3'
     ];
+    $scope.addTodo = function () {
+        $scope.todos.push($scope.todo);
+        $scope.todo = '';
+    };
   });
